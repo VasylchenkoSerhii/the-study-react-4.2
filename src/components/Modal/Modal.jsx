ac4960acfb4259df48a97da1f25dcfd2ef2141f6
Vasylchenko Serhii <darnki@gmail.com>
2022-11-09 17:20:50 +0200

    fixed

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -11,7 +11,7 @@ export default function Modal({ toggleModal, children }) {
         return () => {
             window.removeEventListener('keydown', handleClickByEscape);
         };
-    }, []);
+    });
 
     const  handleClickByEscape = e => {
         if (e.code === "Escape") {
